28101d4763637ac068f022b58037010cd5e9684e
agara <agara04@mail.ru>
2021-02-25 01:14:40 +0500

    Нерабочий класс звезд, недоделанный класс поля

--- classes.py
+++ classes.py
@@ -1,7 +1,9 @@
+import random
+
 import pygame
 
 SIZE = WIDTH, HEIGHT = 930, 780
-RC = 14  # renju_cells
+RC = 15  # renju_cells
 COLOR = pygame.Color(200, 170, 0)
 
 
@@ -22,11 +24,17 @@ class Board:
 
         self.turn = 1
 
+        # rv - right version
+        self.rvx, self.rvy = self.get_size()[0] - self.cell_size, self.get_size()[1] - self.cell_size
+
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
+
+        # rv - right version
+        self.rvx, self.rvy = self.get_size()[0] - self.cell_size, self.get_size()[1] - self.cell_size
 
     def render(self, screen):
         # k - коэффицент толщины рамки
@@ -37,8 +45,8 @@ class Board:
         color = pygame.Color(200, 170, 0)
         hsv = color.hsva
         color.hsva = (int(hsv[0]), int(hsv[1]), int(hsv[2]) - 60, int(hsv[3]))
-        pygame.draw.rect(screen, color, (((width // 2) - 2.5 * self.cell_size + self.left,
-                                          (height // 2) - 2.5 * self.cell_size + self.top),
+        pygame.draw.rect(screen, color, (((width // 2) - 3 * self.cell_size + self.left,
+                                          (height // 2) - 3 * self.cell_size + self.top),
                                          (self.cell_size * 5, self.cell_size * 5)))
 
         # само поле
@@ -47,16 +55,16 @@ class Board:
         for row in self.board:
             cx = self.left
             for elem in row:
-                color = pygame.Color(200, 170, 0)
-                pygame.draw.rect(screen, color,
-                                 ((cx, cy),
-                                  (self.cell_size, self.cell_size)), width=k)
-                # тут затемняется рамка
-                hsv = color.hsva
-                color.hsva = (int(hsv[0]), int(hsv[1]), int(hsv[2] - 30), int(hsv[3]))
-                pygame.draw.rect(screen, color,
-                                 ((cx + k, cy + k),
-                                  (self.cell_size, self.cell_size)), width=k)
+                for shadow_k in range(1, -1, -1):
+                    color = pygame.Color(200, 170, 0)
+                    hsv = color.hsva
+                    color.hsva = (int(hsv[0]), int(hsv[1]), int(hsv[2] - 30 * shadow_k), int(hsv[3]))
+                    # vertical line
+                    pygame.draw.line(screen, color, (cx + k * shadow_k, self.top + k * shadow_k),
+                                     (cx + k * shadow_k, self.rvy + self.top), width=k)
+                    # horizontal line
+                    pygame.draw.line(screen, color, (self.left, cy + shadow_k * k),
+                                     (self.rvx + self.left, cy + k * shadow_k), width=k)
 
                 # На это пока забить можно, это фигура хода в общем
                 if elem == 1:
@@ -78,23 +86,23 @@ class Board:
         for k in range(1, 9, 7):
             pygame.draw.circle(screen, COLOR, (3 * self.cell_size + self.left + 1,
                                                (3 + k) * self.cell_size + self.top + 1), 8)
-            pygame.draw.circle(screen, COLOR, (self.get_size()[0] + self.left - self.cell_size * 3 + 1,
+            pygame.draw.circle(screen, COLOR, (self.get_size()[0] + self.left - self.cell_size * 4 + 1,
                                                (3 + k) * self.cell_size + self.top + 1), 8)
 
     # Возможно это в итоге не пригодится, но сейчас это нужно чтобы понимать как работает игра
     # Отображает цифры и буквы
     def nums_letts(self, screen):
         font = pygame.font.Font(None, 2 * RC)
-        for number in range(1, 16):
-            text = font.render(str(number), True, pygame.Color(200, 170, 0))
-            screen.blit(text, (2 * self.left + self.get_size()[0],
+        for number in range(1, RC + 1):
+            text = font.render(str(number), True, COLOR)
+            screen.blit(text, (2 * self.left + self.rvx,
                                0.5 * self.top + (number - 1) * self.cell_size))
 
         letters = "ABCDEFGHIJKLMNO"
-        for n in range(15):
+        for n in range(RC):
             text = font.render(letters[n], True, COLOR)
-            screen.blit(text, (5 + n * self.cell_size, 2 * self.top + self.get_size()[1]))
-#############################################################################################
+            screen.blit(text, (self.left // 2 + n * self.cell_size,
+                               2 * self.top + self.rvy))
 #############################################################################################
 #############################################################################################
 
@@ -122,6 +130,32 @@ class Board:
         cell = self.get_cell(mouse_pos)
         self.on_click(cell)
 
+########################################################################
+
     # возвращает размер доски
     def get_size(self):
         return self.cell_size * self.width, self.cell_size * self.height
+
+
+#########################################################################
+#########################################################################
+#########################################################################
+
+# класс представляет задний фон из точек, который напоминает мерцающие звезды
+class BackgroundBlick:
+    def __init__(self, stars):
+        self.stars = stars
+        self.positions = [(random.randrange(WIDTH), random.randrange(HEIGHT)) for _ in range(stars)]
+        print(self.positions)
+        self.clock = pygame.time.Clock()
+
+    # хотелось бы, чтобы звезды исчезали и появлялись снова каждые 6 секунд
+    def show_stars(self, screen):
+        for darkness in range(100):
+            for star in self.positions:
+                color = pygame.Color(255, 255, 255)
+                hsv = color.hsva
+                print(hsv[2] - darkness)
+                color.hsva = (hsv[0], hsv[1], hsv[2] - darkness, hsv[3])
+                pygame.draw.rect(screen, color, (star, (2, 2)))
+            self.clock.tick()
--- program.py
+++ program.py
@@ -2,10 +2,11 @@
 # Еще надо добавить кнопку сдроса доски / счеткик каждого из игроков
 import pygame as pg
 
-from classes import Board
+from classes import Board, BackgroundBlick
 
-SIZE = WIDTH, HEIGHT = 930, 780
-RC = 14  # renju_cells
+SIZE = WIDTH, HEIGHT = 1000, 780
+RC = 15  # renju_cells
+FPS = 144
 
 
 def main():
@@ -13,12 +14,14 @@ def main():
     pg.init()
     pg.display.set_caption("Рендзю")
     renju_screen = pg.display.set_mode(SIZE)
+    clock = pg.time.Clock()
     game_running = True
 
     # Доска | RC - количество клеток в рэндзю
     board = Board(RC, RC)
-    board.set_view(RC, 5, (HEIGHT - RC * 2) // RC)
-    print(RC, (HEIGHT - RC * 2) // RC)
+    board.set_view(RC, RC, (HEIGHT - RC * 2) // RC)
+
+    bg = BackgroundBlick(100)
 
     while game_running:
         renju_screen.fill((0, 0, 0))
@@ -31,8 +34,11 @@ def main():
                 board.get_click(event.pos)
                 ...
 
+        bg.show_stars(renju_screen)
         board.render(renju_screen)
         board.nums_letts(renju_screen)
+
+        clock.tick()
         pg.display.flip()
     pg.quit()
 
